b7c7e6a3a34541734ac6e4276d9111fd5a40f04f
Saivenkat1903 <86838056+Saivenkat1903@users.noreply.github.com>
2022-05-21 13:46:55 +0530

    Update Diversity_Calculations.R
    Changed the file references to match the references in repositiory

--- R code/Diversity_Calculations.R
+++ R code/Diversity_Calculations.R
@@ -1,11 +1,11 @@
 #Data upload
-Eco_Sai <- read.csv("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Sai.csv")
+Eco_14m <- read.csv("Eco_Project\\Original_data\\Eco_Data_14m.csv")
 
 
-#Diversity for Sai
-Species_Freq <-data.frame(table(Eco_Sai$taxon_family_name))
+#Diversity for 14m
+Species_Freq <-data.frame(table(Eco_14m$taxon_family_name))
 
-Total <- nrow(Eco_Sai)
+Total <- nrow(Eco_14m)
 
 for (k in seq(1,nrow(Species_Freq))){
   Species_Freq[k,3]=Species_Freq[k,2]/Total
@@ -26,13 +26,13 @@ print(Shanon_Diversity)
 print(Simpson_Diversity)
 rm(list=ls())
 
-#Data upload for Meenakshi
-Eco_Meen <-read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Meenakshi.xlsx")
+#Data upload for 884m
+Eco_884m <-read_excel("Eco_Project\\Original_data\\Eco_Data_884m.xlsx")
 
 
-#Diversity for Meenakshi
-Family_Freq <- data.frame(table(Eco_Meen_Clean$Family))
-Total <- sum(Eco_Meen$`No of observations`)
+#Diversity for 884m
+Family_Freq <- data.frame(table(Eco_884m$Family))
+Total <- sum(Eco_884m$`No of observations`)
 
 for (k in seq(1,nrow(Family_Freq))){
   Family_Freq[k,3]=Family_Freq[k,2]/Total
@@ -51,41 +51,13 @@ print(Shanon_Diversity)
 print(Simpson_Diversity)
 rm(list=ls())
 
-#Data upload for Madhu
-Eco_Madhu <-read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Madhu.xlsx")
+#Data upload for 611m
+Eco_Madhu <-read_excel("Eco_Project\\Original_data\\Eco_Data_611m.xlsx")
 
-#Diversity for Madhu
-Species_Freq <-data.frame(table(Eco_Madhu$taxon_family_name))
+#Diversity for 611m
+Species_Freq <-data.frame(table(Eco_611m$taxon_family_name))
 
-Total <- nrow(Eco_Madhu)
-
-
-for (k in seq(1,nrow(Species_Freq))){
-  Species_Freq[k,3]=Species_Freq[k,2]/Total
-  Species_Freq[k,4]=log(Species_Freq[k,3])
-  Species_Freq[k,5]=Species_Freq[k,3]*Species_Freq[k,4]
-  Species_Freq[k,6]=Species_Freq[k,3]**2
-}
-
-print(Species_Freq)
-
-Shanon_total <- sum(Species_Freq$V5)
-Shanon_Diversity <- Shanon_total*(-1)
-
-Simpson_Total <- sum(Species_Freq$V6)
-Simpson_Diversity <- 1-Simpson_Total
-
-print(Shanon_Diversity)
-print(Simpson_Diversity)
-rm(list=ls())
-
-#Data upload for Revanth
-Eco_Revanth <- read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Revanth.xlsx")
-
-#Diversity for Revanth
-Species_Freq <-data.frame(table(Eco_Revanth$taxon_family_name))
-
-Total <- nrow(Eco_Revanth)
+Total <- nrow(Eco_611m)
 
 
 for (k in seq(1,nrow(Species_Freq))){
@@ -107,41 +79,13 @@ print(Shanon_Diversity)
 print(Simpson_Diversity)
 rm(list=ls())
 
-#Data upload for Manasa
-Eco_Manasa <- read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Manasa.xlsx")
+#Data upload for 498m
+Eco_498m <- read_excel("Eco_Project\\Original_data\\Eco_Data_498m.xlsx")
 
-#Diversity for Manasa
-Species_Freq <-data.frame(table(Eco_Manasa$Family))
+#Diversity for 498m
+Species_Freq <-data.frame(table(Eco_498m$taxon_family_name))
 
-Total <- nrow(Eco_Manasa)
-
-
-for (k in seq(1,nrow(Species_Freq))){
-  Species_Freq[k,3]=Species_Freq[k,2]/Total
-  Species_Freq[k,4]=log(Species_Freq[k,3])
-  Species_Freq[k,5]=Species_Freq[k,3]*Species_Freq[k,4]
-  Species_Freq[k,6]=Species_Freq[k,3]**2
-}
-
-print(Species_Freq)
-
-Shanon_total <- sum(Species_Freq$V5)
-Shanon_Diversity <- Shanon_total*(-1)
-
-Simpson_Total <- sum(Species_Freq$V6)
-Simpson_Diversity <- 1-Simpson_Total
-
-print(Shanon_Diversity)
-print(Simpson_Diversity)
-rm(list=ls())
-
-#Data upload for Srivani
-Eco_Srivani <-read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Srivani.xlsx")
-
-#Diversity for Srivani
-Species_Freq <-data.frame(table(Eco_Srivani$taxon_family_name))
-
-Total <- nrow(Eco_Srivani)
+Total <- nrow(Eco_498m)
 
 
 for (k in seq(1,nrow(Species_Freq))){
@@ -163,20 +107,76 @@ print(Shanon_Diversity)
 print(Simpson_Diversity)
 rm(list=ls())
 
-#Data upload for Rajagopal
-Eco_Raja <- read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Raja.xlsx")
+#Data upload for 457m
+Eco_457m <- read_excel("Eco_Project\\Original_data\\Eco_Data_457m.xlsx")
 
-#Diversity for Rajagopal
-Family_Freq <-data.frame(table(Eco_Raja$taxon_family_name))
+#Diversity for 457m
+Species_Freq <-data.frame(table(Eco_457m$Family))
 
-Total <- nrow(Eco_Raja)
+Total <- nrow(Eco_457m)
 
 
-for (k in seq(1,nrow(Eco_Raja_Clean))){
-  Eco_Raja[k,4]=Eco_Raja[k,3]/Total
-  Eco_Raja[k,5]=log(Eco_Raja[k,4])
-  Eco_Raja[k,6]=Eco_Raja[k,5]*Eco_Raja[k,4]
-  Eco_Raja[k,7]=Eco_Raja[k,4]**2
+for (k in seq(1,nrow(Species_Freq))){
+  Species_Freq[k,3]=Species_Freq[k,2]/Total
+  Species_Freq[k,4]=log(Species_Freq[k,3])
+  Species_Freq[k,5]=Species_Freq[k,3]*Species_Freq[k,4]
+  Species_Freq[k,6]=Species_Freq[k,3]**2
+}
+
+print(Species_Freq)
+
+Shanon_total <- sum(Species_Freq$V5)
+Shanon_Diversity <- Shanon_total*(-1)
+
+Simpson_Total <- sum(Species_Freq$V6)
+Simpson_Diversity <- 1-Simpson_Total
+
+print(Shanon_Diversity)
+print(Simpson_Diversity)
+rm(list=ls())
+
+#Data upload for 240m
+Eco_240m<-read_excel("Eco_Project\\Original_data\\Eco_Data_240m.xlsx")
+
+#Diversity for 240m
+Species_Freq <-data.frame(table(Eco_240m$taxon_family_name))
+
+Total <- nrow(Eco_240m)
+
+
+for (k in seq(1,nrow(Species_Freq))){
+  Species_Freq[k,3]=Species_Freq[k,2]/Total
+  Species_Freq[k,4]=log(Species_Freq[k,3])
+  Species_Freq[k,5]=Species_Freq[k,3]*Species_Freq[k,4]
+  Species_Freq[k,6]=Species_Freq[k,3]**2
+}
+
+print(Species_Freq)
+
+Shanon_total <- sum(Species_Freq$V5)
+Shanon_Diversity <- Shanon_total*(-1)
+
+Simpson_Total <- sum(Species_Freq$V6)
+Simpson_Diversity <- 1-Simpson_Total
+
+print(Shanon_Diversity)
+print(Simpson_Diversity)
+rm(list=ls())
+
+#Data upload for 131m
+Eco_131m <- read_excel("Eco_Project\\Original_data\\Eco_Data_131m.xlsx")
+
+#Diversity for 131m
+Family_Freq <-data.frame(table(Eco_131m$taxon_family_name))
+
+Total <- nrow(Eco_131m)
+
+
+for (k in seq(1,nrow(Eco_131m))){
+  Eco_131m[k,4]=Eco_131m[k,3]/Total
+  Eco_131m[k,5]=log(Eco_131m[k,4])
+  Eco_131m[k,6]=Eco_131m[k,5]*Eco_131m[k,4]
+  Eco_131m[k,7]=Eco_131m[k,4]**2
 }
 
 for (k in seq(1,nrow(Family_Freq))){
@@ -186,7 +186,7 @@ for (k in seq(1,nrow(Family_Freq))){
   Family_Freq[k,6]=Family_Freq[k,3]**2
 }
 
-print(Eco_Raja)
+print(Eco_131m)
 
 Shanon_total <- sum(Family_Freq[,5])
 Shanon_Diversity <- Shanon_total*(-1)
@@ -198,13 +198,13 @@ print(Shanon_Diversity)
 print(Simpson_Diversity)
 rm(list=ls())
 
-#Data upload for Sabdhayani
-Eco_Sabdhayani <-read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Sabdhayani.xlsx")
+#Data upload for 52m
+Eco_52m <-read_excel("Eco_Project\\Original_data\\Eco_Data_52m.xlsx")
 
-#Diversity for Sabdhayani
+#Diversity for 52m
 Species_Freq <-data.frame(table(Eco_Sabdhayani$taxon_family_name))
 
-Total <- nrow(Eco_Sabdhayani)
+Total <- nrow(Eco_52m)
 
 
 for (k in seq(1,nrow(Species_Freq))){
@@ -226,41 +226,13 @@ print(Shanon_Diversity)
 print(Simpson_Diversity)
 rm(list=ls())
 
-#Data upload for Rushdra
-Eco_Rushdra <-read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Rushdra.xlsx")
+#Data upload for 38m
+Eco_38m <-read_excel("Eco_Project\\Original_data\\Eco_Data_38m.xlsx")
 
-#Diversity for Rushdra
-Species_Freq <-data.frame(table(Eco_Rushdra$taxon_family_name))
+#Diversity for 38m
+Species_Freq <-data.frame(table(Eco_38m$taxon_family_name))
 
-Total <- nrow(Eco_Rushdra)
-
-
-for (k in seq(1,nrow(Species_Freq))){
-  Species_Freq[k,3]=Species_Freq[k,2]/Total
-  Species_Freq[k,4]=log(Species_Freq[k,3])
-  Species_Freq[k,5]=Species_Freq[k,3]*Species_Freq[k,4]
-  Species_Freq[k,6]=Species_Freq[k,3]**2
-}
-
-print(Species_Freq)
-
-Shanon_total <- sum(Species_Freq$V5)
-Shanon_Diversity <- Shanon_total*(-1)
-
-Simpson_Total <- sum(Species_Freq$V6)
-Simpson_Diversity <- 1-Simpson_Total
-
-print(Shanon_Diversity)
-print(Simpson_Diversity)
-rm(list=ls())
-
-#Data upload for Bhavana
-Eco_Bhavana <- read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Bhavana.xlsx")
-
-#Diversity for Bhavana
-Species_Freq <-data.frame(table(Eco_Bhavana$taxon_family_name))
-
-Total <- nrow(Eco_Bhavana)
+Total <- nrow(Eco_38m)
 
 
 for (k in seq(1,nrow(Species_Freq))){
@@ -282,41 +254,13 @@ print(Shanon_Diversity)
 print(Simpson_Diversity)
 rm(list=ls())
 
-#Data upload for Adhina
-Eco_Adhina <- read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Adhina.xlsx")
+#Data upload for 37m
+Eco_37m <- read_excel("Eco_Project\\Original_data\\Eco_Data_37m.xlsx")
 
-#Diversity for Adhina
-Species_Freq <-data.frame(table(Eco_Adhina$taxon_family_name))
+#Diversity for 37m
+Species_Freq <-data.frame(table(Eco_37m$taxon_family_name))
 
-Total <- nrow(Eco_Adhina)
-
-
-for (k in seq(1,nrow(Species_Freq))){
-  Species_Freq[k,3]=Species_Freq[k,2]/Total
-  Species_Freq[k,4]=log(Species_Freq[k,3])
-  Species_Freq[k,5]=Species_Freq[k,3]*Species_Freq[k,4]
-  Species_Freq[k,6]=Species_Freq[k,3]**2
-}
-
-print(Species_Freq)
-
-Shanon_total <- sum(Species_Freq$V5)
-Shanon_Diversity <- Shanon_total*(-1)
-
-Simpson_Total <- sum(Species_Freq$V6)
-Simpson_Diversity <- 1-Simpson_Total
-
-print(Shanon_Diversity)
-print(Simpson_Diversity)
-rm(list=ls())
-
-#Data upload for RC Vishnu
-Eco_RC <- read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_RC.xlsx")
-
-#Diversity for RC Vishnu
-Species_Freq <-data.frame(table(Eco_RC$taxon_family_name))
-
-Total <- nrow(Eco_RC)
+Total <- nrow(Eco_37m)
 
 
 for (k in seq(1,nrow(Species_Freq))){
@@ -338,12 +282,68 @@ print(Shanon_Diversity)
 print(Simpson_Diversity)
 rm(list=ls())
 
-#Data upload for Mukesh
-Eco_Mukesh <-read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Mukesh.xlsx")
+#Data upload for 30m
+Eco_30m <- read_excel("Eco_Project\\Original_data\\Eco_Data_30m.xlsx")
 
-#Diversity for Mukesh
+#Diversity for 30m
+Species_Freq <-data.frame(table(Eco_30m$taxon_family_name))
 
-Family_Freq <- data.frame(table(Eco_Mukesh_Clean$taxon_family_name))
+Total <- nrow(Eco_30m)
+
+
+for (k in seq(1,nrow(Species_Freq))){
+  Species_Freq[k,3]=Species_Freq[k,2]/Total
+  Species_Freq[k,4]=log(Species_Freq[k,3])
+  Species_Freq[k,5]=Species_Freq[k,3]*Species_Freq[k,4]
+  Species_Freq[k,6]=Species_Freq[k,3]**2
+}
+
+print(Species_Freq)
+
+Shanon_total <- sum(Species_Freq$V5)
+Shanon_Diversity <- Shanon_total*(-1)
+
+Simpson_Total <- sum(Species_Freq$V6)
+Simpson_Diversity <- 1-Simpson_Total
+
+print(Shanon_Diversity)
+print(Simpson_Diversity)
+rm(list=ls())
+
+#Data upload for 21m
+Eco_21m <- read_excel("Eco_Project\\Original_data\\Eco_Data_21m.xlsx")
+
+#Diversity for 21m
+Species_Freq <-data.frame(table(Eco_21m$taxon_family_name))
+
+Total <- nrow(Eco_21m)
+
+
+for (k in seq(1,nrow(Species_Freq))){
+  Species_Freq[k,3]=Species_Freq[k,2]/Total
+  Species_Freq[k,4]=log(Species_Freq[k,3])
+  Species_Freq[k,5]=Species_Freq[k,3]*Species_Freq[k,4]
+  Species_Freq[k,6]=Species_Freq[k,3]**2
+}
+
+print(Species_Freq)
+
+Shanon_total <- sum(Species_Freq$V5)
+Shanon_Diversity <- Shanon_total*(-1)
+
+Simpson_Total <- sum(Species_Freq$V6)
+Simpson_Diversity <- 1-Simpson_Total
+
+print(Shanon_Diversity)
+print(Simpson_Diversity)
+rm(list=ls())
+
+#Data upload for 16m
+Eco_16m <-read_excel("Eco_Project\\Original_data\\Eco_Data_16m.xlsx")
+
+#Diversity for 16m
+
+Family_Freq <- data.frame(table(Eco_16m$taxon_family_name))
 
 Total <- sum(Family_Freq$Freq)
 
@@ -354,14 +354,14 @@ for (k in seq(1,nrow(Family_Freq))){
   Family_Freq[k,6]=Family_Freq[k,3]**2
 }
 
-for (k in seq(1,nrow(Eco_Mukesh))){
-  Eco_Mukesh[k,6]=Eco_Mukesh[k,4]/Total
-  Eco_Mukesh[k,7]=log(Eco_Mukesh[k,6])
-  Eco_Mukesh[k,8]=Eco_Mukesh[k,6]*Eco_Mukesh[k,7]
-  Eco_Mukesh[k,9]=Eco_Mukesh[k,6]**2
+for (k in seq(1,nrow(Eco_16m))){
+  Eco_16m[k,6]=Eco_16m[k,4]/Total
+  Eco_16m[k,7]=log(Eco_16m[k,6])
+  Eco_16m[k,8]=Eco_16m[k,6]*Eco_16m[k,7]
+  Eco_16m[k,9]=Eco_16m[k,6]**2
 }
 
-print(Eco_Mukesh)
+print(Eco_16m)
 
 Shanon_total <- sum(Family_Freq[,5])
 Shanon_Diversity <- Shanon_total*(-1)
@@ -374,12 +374,12 @@ print(Simpson_Diversity)
 rm(list=ls())
 
 
-#Data upload for Shashank
-Eco_Shashank <- read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Eco_Data_Shashank.xlsx")
+#Data upload for 16m1
+Eco_16m1 <- read_excel("Eco_Project\\Original_data\\Eco_Data_16m1.xlsx")
 
 #Diversity for Shashank
 
-Family_Freq <- data.frame(table(Eco_Shashank_Clean$taxon_family_name))
+Family_Freq <- data.frame(table(Eco_16m1$taxon_family_name))
 
 Total <- sum(Family_Freq$Freq)
 
@@ -390,11 +390,11 @@ for (k in seq(1,nrow(Family_Freq))){
   Family_Freq[k,6]=Family_Freq[k,3]**2
 }
 
-for (k in seq(1,nrow(Eco_Mukesh))){
-  Eco_Mukesh[k,6]=Eco_Mukesh[k,4]/Total
-  Eco_Mukesh[k,7]=log(Eco_Mukesh[k,6])
-  Eco_Mukesh[k,8]=Eco_Mukesh[k,6]*Eco_Mukesh[k,7]
-  Eco_Mukesh[k,9]=Eco_Mukesh[k,6]**2
+for (k in seq(1,nrow(Eco_16m1))){
+  Eco_16m1[k,6]=Eco_16m1[k,4]/Total
+  Eco_16m1[k,7]=log(Eco_16m1[k,6])
+  Eco_16m1[k,8]=Eco_16m1[k,6]*Eco_16m1[k,7]
+  Eco_16m1[k,9]=Eco_16m1[k,6]**2
 }
 
 
@@ -409,8 +409,8 @@ print(Simpson_Diversity)
 rm(list=ls())
 
 
-#subgroup stuff
-Family_Freq_below <- read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Master_data_family.xlsx")
+#subgroup below
+Family_Freq_below <- read_excel("Eco_Project\\Processed_data\\Master_data_family.xlsx")
 
 Total_below <-sum(Family_Freq_below$Freq)
 
@@ -421,10 +421,10 @@ for (k in seq(1,nrow(Family_Freq_below))){
   Family_Freq_below[k,7]=Family_Freq_below[k,4]**2
 }
 
-write.xlsx(Family_Freq_below,"D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Family_Freq_Below.xlsx")
+write.xlsx(Family_Freq_below,"Eco_Project\\Processed_data\\\\Family_Freq_Below.xlsx")
 
-#more subgroup stuff
-Total_family_above <- read_excel("D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Total_Observations_Above (Species).xlsx")
+#subgroup above
+Total_family_above <- read_excel("Eco_Project\\Processed_data\\Total_Observations_Above (Species).xlsx")
 
 Family_Freq_above <-data.frame(table(Total_family_above$Species))
 
@@ -437,6 +437,6 @@ for (k in seq(1,nrow(Family_Freq_above))){
   Family_Freq_above[k,6]=Family_Freq_above[k,3]**2
 }
 
-write.xlsx(Family_Freq_above,"D:\\Saivenkat\\College shizzle dizzle\\Semester 3\\BIO211\\Species_Freq_Above.xlsx")
+write.xlsx(Family_Freq_above,"Eco_Project\\Processed_data\\Species_Freq_Above.xlsx")
 
 
